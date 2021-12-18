6a2d6cc50f5a7b45e33cc56cfc94b37c2fd8ac34
Duke Luo <dukeluo@outlook.com>
2021-12-18 03:48:16 -0500

    feat: match by filename rather than fex

--- tests/lib/rules/match-with-fex.js
+++ tests/lib/rules/match-with-fex.js
@@ -9,35 +9,65 @@ const RuleTester = require('eslint').RuleTester;
 
 const ruleTester = new RuleTester();
 
-ruleTester.run('match-with-fex', rule, {
+ruleTester.run("['always', { '*.js': '**/__tests__/' }]", rule, {
   valid: [
     {
       code: "var foo = 'bar';",
       filename: '/__tests__/foo.test.js',
-      options: ['always', { '.js': '**/__tests__/' }],
+      options: ['always', { '*.js': '**/__tests__/' }],
     },
     {
       code: "var foo = 'bar';",
       filename: '__tests__/foo.test.js',
-      options: ['always', { '.js': '**/__tests__/' }],
+      options: ['always', { '*.js': '**/__tests__/' }],
     },
     {
       code: "var foo = 'bar';",
       filename: 'bar/__tests__/foo.test.js',
-      options: ['always', { '.js': '**/__tests__/' }],
+      options: ['always', { '*.js': '**/__tests__/' }],
     },
     {
       code: "var foo = 'bar';",
       filename: '/bar/__tests__/foo.test.js',
-      options: ['always', { '.js': '**/__tests__/' }],
+      options: ['always', { '*.js': '**/__tests__/' }],
     },
   ],
 
   invalid: [
     {
       code: "var foo = 'bar';",
+      filename: '/bar/__test__/foo.test.js',
+      options: ['always', { '*.js': '**/__tests__/' }],
+      errors: [
+        {
+          message:
+            'The folder of the file "/bar/__test__/foo.test.js" does not match "**/__tests__/"',
+          column: 1,
+          line: 1,
+        },
+      ],
+    },
+  ],
+});
+
+ruleTester.run("['always', { '*.js': '*/__tests__/' }]", rule, {
+  valid: [
+    {
+      code: "var foo = 'bar';",
+      filename: '/__tests__/foo.test.js',
+      options: ['always', { '*.js': '*/__tests__/' }],
+    },
+    {
+      code: "var foo = 'bar';",
+      filename: 'bar/__tests__/foo.test.js',
+      options: ['always', { '*.js': '*/__tests__/' }],
+    },
+  ],
+  invalid: [
+    {
+      code: "var foo = 'bar';",
       filename: '/__test__/foo.test.js',
-      options: ['always', { '.js': '*/__tests__/' }],
+      options: ['always', { '*.js': '*/__tests__/' }],
       errors: [
         {
           message:
@@ -47,5 +77,114 @@ ruleTester.run('match-with-fex', rule, {
         },
       ],
     },
+    {
+      code: "var foo = 'bar';",
+      filename: '/bar/__tests__/foo.test.js',
+      options: ['always', { '*.js': '*/__tests__/' }],
+      errors: [
+        {
+          message:
+            'The folder of the file "/bar/__tests__/foo.test.js" does not match "*/__tests__/"',
+          column: 1,
+          line: 1,
+        },
+      ],
+    },
+    {
+      code: "var foo = 'bar';",
+      filename: '__tests__/foo.test.js',
+      options: ['always', { '*.js': '*/__tests__/' }],
+      errors: [
+        {
+          message:
+            'The folder of the file "__tests__/foo.test.js" does not match "*/__tests__/"',
+          column: 1,
+          line: 1,
+        },
+      ],
+    },
   ],
 });
+
+ruleTester.run(
+  "['always', { '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }]",
+  rule,
+  {
+    valid: [
+      {
+        code: "var foo = 'bar';",
+        filename: 'bar/__tests__/foo.test.js',
+        options: ['always', { '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'bar/__tests__/foo.test.jsx',
+        options: ['always', { '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'bar/__tests__/foo.test.ts',
+        options: ['always', { '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'bar/__tests__/foo.test.tsx',
+        options: ['always', { '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
+      },
+    ],
+    invalid: [
+      {
+        code: "var foo = 'bar';",
+        filename: 'bar/_tests_/foo.test.js',
+        options: ['always', { '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
+        errors: [
+          {
+            message:
+              'The folder of the file "bar/_tests_/foo.test.js" does not match "*/__tests__/"',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'bar/_tests_/foo.test.jsx',
+        options: ['always', { '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
+        errors: [
+          {
+            message:
+              'The folder of the file "bar/_tests_/foo.test.jsx" does not match "*/__tests__/"',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'bar/_tests_/foo.test.ts',
+        options: ['always', { '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
+        errors: [
+          {
+            message:
+              'The folder of the file "bar/_tests_/foo.test.ts" does not match "*/__tests__/"',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'bar/_tests_/foo.test.tsx',
+        options: ['always', { '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
+        errors: [
+          {
+            message:
+              'The folder of the file "bar/_tests_/foo.test.tsx" does not match "*/__tests__/"',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+    ],
+  }
+);
