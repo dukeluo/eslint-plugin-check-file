fd682f13fba7ea615c607acd653be52f06b7f193
Duke Luo <dukeluo@outlook.com>
2021-12-18 04:51:50 -0500

    refactor: remove useless config

--- tests/lib/rules/match-with-fex.js
+++ tests/lib/rules/match-with-fex.js
@@ -9,27 +9,27 @@ const RuleTester = require('eslint').RuleTester;
 
 const ruleTester = new RuleTester();
 
-ruleTester.run("['always', { '*.js': '**/__tests__/' }]", rule, {
+ruleTester.run("[{ '*.js': '**/__tests__/' }]", rule, {
   valid: [
     {
       code: "var foo = 'bar';",
       filename: '/__tests__/foo.test.js',
-      options: ['always', { '*.js': '**/__tests__/' }],
+      options: [{ '*.js': '**/__tests__/' }],
     },
     {
       code: "var foo = 'bar';",
       filename: '__tests__/foo.test.js',
-      options: ['always', { '*.js': '**/__tests__/' }],
+      options: [{ '*.js': '**/__tests__/' }],
     },
     {
       code: "var foo = 'bar';",
       filename: 'bar/__tests__/foo.test.js',
-      options: ['always', { '*.js': '**/__tests__/' }],
+      options: [{ '*.js': '**/__tests__/' }],
     },
     {
       code: "var foo = 'bar';",
       filename: '/bar/__tests__/foo.test.js',
-      options: ['always', { '*.js': '**/__tests__/' }],
+      options: [{ '*.js': '**/__tests__/' }],
     },
   ],
 
@@ -37,7 +37,7 @@ ruleTester.run("['always', { '*.js': '**/__tests__/' }]", rule, {
     {
       code: "var foo = 'bar';",
       filename: '/bar/__test__/foo.test.js',
-      options: ['always', { '*.js': '**/__tests__/' }],
+      options: [{ '*.js': '**/__tests__/' }],
       errors: [
         {
           message:
@@ -50,24 +50,24 @@ ruleTester.run("['always', { '*.js': '**/__tests__/' }]", rule, {
   ],
 });
 
-ruleTester.run("['always', { '*.js': '*/__tests__/' }]", rule, {
+ruleTester.run("[{ '*.js': '*/__tests__/' }]", rule, {
   valid: [
     {
       code: "var foo = 'bar';",
       filename: '/__tests__/foo.test.js',
-      options: ['always', { '*.js': '*/__tests__/' }],
+      options: [{ '*.js': '*/__tests__/' }],
     },
     {
       code: "var foo = 'bar';",
       filename: 'bar/__tests__/foo.test.js',
-      options: ['always', { '*.js': '*/__tests__/' }],
+      options: [{ '*.js': '*/__tests__/' }],
     },
   ],
   invalid: [
     {
       code: "var foo = 'bar';",
       filename: '/__test__/foo.test.js',
-      options: ['always', { '*.js': '*/__tests__/' }],
+      options: [{ '*.js': '*/__tests__/' }],
       errors: [
         {
           message:
@@ -80,7 +80,7 @@ ruleTester.run("['always', { '*.js': '*/__tests__/' }]", rule, {
     {
       code: "var foo = 'bar';",
       filename: '/bar/__tests__/foo.test.js',
-      options: ['always', { '*.js': '*/__tests__/' }],
+      options: [{ '*.js': '*/__tests__/' }],
       errors: [
         {
           message:
@@ -93,7 +93,7 @@ ruleTester.run("['always', { '*.js': '*/__tests__/' }]", rule, {
     {
       code: "var foo = 'bar';",
       filename: '__tests__/foo.test.js',
-      options: ['always', { '*.js': '*/__tests__/' }],
+      options: [{ '*.js': '*/__tests__/' }],
       errors: [
         {
           message:
@@ -106,119 +106,106 @@ ruleTester.run("['always', { '*.js': '*/__tests__/' }]", rule, {
   ],
 });
 
-ruleTester.run(
-  "['always', { '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }]",
-  rule,
-  {
-    valid: [
-      {
-        code: "var foo = 'bar';",
-        filename: 'bar/__tests__/foo.test.js',
-        options: ['always', { '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
-      },
-      {
-        code: "var foo = 'bar';",
-        filename: 'bar/__tests__/foo.test.jsx',
-        options: ['always', { '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
-      },
-      {
-        code: "var foo = 'bar';",
-        filename: 'bar/__tests__/foo.test.ts',
-        options: ['always', { '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
-      },
-      {
-        code: "var foo = 'bar';",
-        filename: 'bar/__tests__/foo.test.tsx',
-        options: ['always', { '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
-      },
-    ],
-    invalid: [
-      {
-        code: "var foo = 'bar';",
-        filename: 'bar/_tests_/foo.test.js',
-        options: ['always', { '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
-        errors: [
-          {
-            message:
-              'The folder of the file "bar/_tests_/foo.test.js" does not match "*/__tests__/"',
-            column: 1,
-            line: 1,
-          },
-        ],
-      },
-      {
-        code: "var foo = 'bar';",
-        filename: 'bar/_tests_/foo.test.jsx',
-        options: ['always', { '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
-        errors: [
-          {
-            message:
-              'The folder of the file "bar/_tests_/foo.test.jsx" does not match "*/__tests__/"',
-            column: 1,
-            line: 1,
-          },
-        ],
-      },
-      {
-        code: "var foo = 'bar';",
-        filename: 'bar/_tests_/foo.test.ts',
-        options: ['always', { '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
-        errors: [
-          {
-            message:
-              'The folder of the file "bar/_tests_/foo.test.ts" does not match "*/__tests__/"',
-            column: 1,
-            line: 1,
-          },
-        ],
-      },
-      {
-        code: "var foo = 'bar';",
-        filename: 'bar/_tests_/foo.test.tsx',
-        options: ['always', { '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
-        errors: [
-          {
-            message:
-              'The folder of the file "bar/_tests_/foo.test.tsx" does not match "*/__tests__/"',
-            column: 1,
-            line: 1,
-          },
-        ],
-      },
-    ],
-  }
-);
+ruleTester.run("[{ '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }]", rule, {
+  valid: [
+    {
+      code: "var foo = 'bar';",
+      filename: 'bar/__tests__/foo.test.js',
+      options: [{ '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
+    },
+    {
+      code: "var foo = 'bar';",
+      filename: 'bar/__tests__/foo.test.jsx',
+      options: [{ '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
+    },
+    {
+      code: "var foo = 'bar';",
+      filename: 'bar/__tests__/foo.test.ts',
+      options: [{ '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
+    },
+    {
+      code: "var foo = 'bar';",
+      filename: 'bar/__tests__/foo.test.tsx',
+      options: [{ '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
+    },
+  ],
+  invalid: [
+    {
+      code: "var foo = 'bar';",
+      filename: 'bar/_tests_/foo.test.js',
+      options: [{ '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
+      errors: [
+        {
+          message:
+            'The folder of the file "bar/_tests_/foo.test.js" does not match "*/__tests__/"',
+          column: 1,
+          line: 1,
+        },
+      ],
+    },
+    {
+      code: "var foo = 'bar';",
+      filename: 'bar/_tests_/foo.test.jsx',
+      options: [{ '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
+      errors: [
+        {
+          message:
+            'The folder of the file "bar/_tests_/foo.test.jsx" does not match "*/__tests__/"',
+          column: 1,
+          line: 1,
+        },
+      ],
+    },
+    {
+      code: "var foo = 'bar';",
+      filename: 'bar/_tests_/foo.test.ts',
+      options: [{ '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
+      errors: [
+        {
+          message:
+            'The folder of the file "bar/_tests_/foo.test.ts" does not match "*/__tests__/"',
+          column: 1,
+          line: 1,
+        },
+      ],
+    },
+    {
+      code: "var foo = 'bar';",
+      filename: 'bar/_tests_/foo.test.tsx',
+      options: [{ '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
+      errors: [
+        {
+          message:
+            'The folder of the file "bar/_tests_/foo.test.tsx" does not match "*/__tests__/"',
+          column: 1,
+          line: 1,
+        },
+      ],
+    },
+  ],
+});
 
 ruleTester.run(
-  "['always', { '*.test.js': '*/__tests__/', '*.test.ts': '*/__tests__/' }]",
+  "[{ '*.test.js': '*/__tests__/', '*.test.ts': '*/__tests__/' }]",
   rule,
   {
     valid: [
       {
         code: "var foo = 'bar';",
         filename: 'bar/__tests__/foo.test.js',
-        options: [
-          'always',
-          { '*.test.js': '*/__tests__/', '*.test.ts': '*/__tests__/' },
-        ],
+        options: [{ '*.test.js': '*/__tests__/', '*.test.ts': '*/__tests__/' }],
       },
       {
         code: "var foo = 'bar';",
         filename: 'bar/__tests__/foo.test.ts',
-        options: [
-          'always',
-          { '*.test.js': '*/__tests__/', '*.test.ts': '*/__tests__/' },
-        ],
+        options: [{ '*.test.js': '*/__tests__/', '*.test.ts': '*/__tests__/' }],
       },
     ],
     invalid: [
       {
         code: "var foo = 'bar';",
         filename: 'bar/_tests_/foo.test.js',
-        options: [
-          'always',
-          { '*.test.js': '*/__tests__/', '*.test.ts': '*/__tests__/' },
-        ],
+        options: [{ '*.test.js': '*/__tests__/', '*.test.ts': '*/__tests__/' }],
         errors: [
           {
             message:
@@ -231,10 +218,7 @@ ruleTester.run(
       {
         code: "var foo = 'bar';",
         filename: 'bar/_tests_/foo.test.ts',
-        options: [
-          'always',
-          { '*.test.js': '*/__tests__/', '*.test.ts': '*/__tests__/' },
-        ],
+        options: [{ '*.test.js': '*/__tests__/', '*.test.ts': '*/__tests__/' }],
         errors: [
           {
             message:
@@ -253,10 +237,7 @@ ruleTester.run('fex that has not been set', rule, {
     {
       code: "var foo = 'bar';",
       filename: '/bar/__test__/foo.test.ts',
-      options: [
-        'always',
-        { '*.js': '**/__tests__/', '*.jsx': '**/__tests__/' },
-      ],
+      options: [{ '*.js': '**/__tests__/', '*.jsx': '**/__tests__/' }],
     },
   ],
   invalid: [],
