c804992fcc05ebb1cb097970cba8881861e40382
Duke Luo <dukeluo@outlook.com>
2022-07-03 18:00:54 +0800

    feat: rule filename-naming-convention can specify target file by file path

--- tests/lib/rules/filename-naming-convention.js
+++ tests/lib/rules/filename-naming-convention.js
@@ -654,3 +654,352 @@ ruleTester.run(
     ],
   }
 );
+
+ruleTester.run(
+  "filename-naming-convention with option: [{ 'src/services/*.js': 'PASCAL_CASE', 'src/composables/*.js': 'CAMEL_CASE' }]",
+  rule,
+  {
+    valid: [
+      {
+        code: "var foo = 'bar';",
+        filename: 'src/services/DownloadService.js',
+        options: [
+          {
+            'src/services/*.js': 'PASCAL_CASE',
+            'src/composables/*.js': 'CAMEL_CASE',
+          },
+        ],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'src/composables/useDownloadApi.js',
+        options: [
+          {
+            'src/services/*.js': 'PASCAL_CASE',
+            'src/composables/*.js': 'CAMEL_CASE',
+          },
+        ],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'src/utils/CalculatePrice.ts',
+        options: [
+          {
+            'src/services/*.js': 'PASCAL_CASE',
+            'src/composables/*.js': 'CAMEL_CASE',
+          },
+        ],
+      },
+    ],
+
+    invalid: [
+      {
+        code: "var foo = 'bar';",
+        filename: 'src/services/downloadService.js',
+        options: [
+          {
+            'src/services/*.js': 'PASCAL_CASE',
+            'src/composables/*.js': 'CAMEL_CASE',
+          },
+        ],
+        errors: [
+          {
+            message:
+              'The filename "src/services/downloadService.js" does not match the "PASCAL_CASE" style',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'src/services/downloadservice.js',
+        options: [
+          {
+            'src/services/*.js': 'PASCAL_CASE',
+            'src/composables/*.js': 'CAMEL_CASE',
+          },
+        ],
+        errors: [
+          {
+            message:
+              'The filename "src/services/downloadservice.js" does not match the "PASCAL_CASE" style',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'src/services/download_service.js',
+        options: [
+          {
+            'src/services/*.js': 'PASCAL_CASE',
+            'src/composables/*.js': 'CAMEL_CASE',
+          },
+        ],
+        errors: [
+          {
+            message:
+              'The filename "src/services/download_service.js" does not match the "PASCAL_CASE" style',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'src/services/download-service.js',
+        options: [
+          {
+            'src/services/*.js': 'PASCAL_CASE',
+            'src/composables/*.js': 'CAMEL_CASE',
+          },
+        ],
+        errors: [
+          {
+            message:
+              'The filename "src/services/download-service.js" does not match the "PASCAL_CASE" style',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'src/services/DOWNLOAD_SERVICE.js',
+        options: [
+          {
+            'src/services/*.js': 'PASCAL_CASE',
+            'src/composables/*.js': 'CAMEL_CASE',
+          },
+        ],
+        errors: [
+          {
+            message:
+              'The filename "src/services/DOWNLOAD_SERVICE.js" does not match the "PASCAL_CASE" style',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'src/services/downloadService.service.js',
+        options: [
+          {
+            'src/services/*.js': 'PASCAL_CASE',
+            'src/composables/*.js': 'CAMEL_CASE',
+          },
+        ],
+        errors: [
+          {
+            message:
+              'The filename "src/services/downloadService.service.js" does not match the "PASCAL_CASE" style',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'src/composables/UseDownloadApi.js',
+        options: [
+          {
+            'src/services/*.js': 'PASCAL_CASE',
+            'src/composables/*.js': 'CAMEL_CASE',
+          },
+        ],
+        errors: [
+          {
+            message:
+              'The filename "src/composables/UseDownloadApi.js" does not match the "CAMEL_CASE" style',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'src/composables/USE_DOWNLOAD_API.js',
+        options: [
+          {
+            'src/services/*.js': 'PASCAL_CASE',
+            'src/composables/*.js': 'CAMEL_CASE',
+          },
+        ],
+        errors: [
+          {
+            message:
+              'The filename "src/composables/USE_DOWNLOAD_API.js" does not match the "CAMEL_CASE" style',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'src/composables/use-download-api.js',
+        options: [
+          {
+            'src/services/*.js': 'PASCAL_CASE',
+            'src/composables/*.js': 'CAMEL_CASE',
+          },
+        ],
+        errors: [
+          {
+            message:
+              'The filename "src/composables/use-download-api.js" does not match the "CAMEL_CASE" style',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'src/composables/use_download_api.js',
+        options: [
+          {
+            'src/services/*.js': 'PASCAL_CASE',
+            'src/composables/*.js': 'CAMEL_CASE',
+          },
+        ],
+        errors: [
+          {
+            message:
+              'The filename "src/composables/use_download_api.js" does not match the "CAMEL_CASE" style',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'src/composables/useDownloadApi.composable.js',
+        options: [
+          {
+            'src/services/*.js': 'PASCAL_CASE',
+            'src/composables/*.js': 'CAMEL_CASE',
+          },
+        ],
+        errors: [
+          {
+            message:
+              'The filename "src/composables/useDownloadApi.composable.js" does not match the "CAMEL_CASE" style',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+    ],
+  }
+);
+
+ruleTester.run(
+  "filename-naming-convention with option: [{ '**/*.js': 'CAMEL_CASE', '**/*.jsx': 'CAMEL_CASE' }]",
+  rule,
+  {
+    valid: [
+      {
+        code: "var foo = 'bar';",
+        filename: 'src/components/login.jsx',
+        options: [{ '**/*.js': 'CAMEL_CASE', '**/*.jsx': 'CAMEL_CASE' }],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'src/login.jsx',
+        options: [{ '**/*.js': 'CAMEL_CASE', '**/*.jsx': 'CAMEL_CASE' }],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'login.jsx',
+        options: [{ '**/*.js': 'CAMEL_CASE', '**/*.jsx': 'CAMEL_CASE' }],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'src/components/logout.jsx',
+        options: [{ '**/*.js': 'CAMEL_CASE', '**/*.jsx': 'CAMEL_CASE' }],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'src/logout.jsx',
+        options: [{ '**/*.js': 'CAMEL_CASE', '**/*.jsx': 'CAMEL_CASE' }],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'logout.jsx',
+        options: [{ '**/*.js': 'CAMEL_CASE', '**/*.jsx': 'CAMEL_CASE' }],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'src/utils/calculatePrice.js',
+        options: [{ '**/*.js': 'CAMEL_CASE', '**/*.jsx': 'CAMEL_CASE' }],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'src/calculatePrice.js',
+        options: [{ '**/*.js': 'CAMEL_CASE', '**/*.jsx': 'CAMEL_CASE' }],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'calculatePrice.js',
+        options: [{ '**/*.js': 'CAMEL_CASE', '**/*.jsx': 'CAMEL_CASE' }],
+      },
+    ],
+
+    invalid: [
+      {
+        code: "var foo = 'bar';",
+        filename: 'src/utils/CalculatePrice.js',
+        options: [{ '**/*.js': 'CAMEL_CASE', '**/*.jsx': 'CAMEL_CASE' }],
+        errors: [
+          {
+            message:
+              'The filename "src/utils/CalculatePrice.js" does not match the "CAMEL_CASE" style',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'src/utils/calculate_price.js',
+        options: [{ '**/*.js': 'CAMEL_CASE', '**/*.jsx': 'CAMEL_CASE' }],
+        errors: [
+          {
+            message:
+              'The filename "src/utils/calculate_price.js" does not match the "CAMEL_CASE" style',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'src/utils/calculate-price.js',
+        options: [{ '**/*.js': 'CAMEL_CASE', '**/*.jsx': 'CAMEL_CASE' }],
+        errors: [
+          {
+            message:
+              'The filename "src/utils/calculate-price.js" does not match the "CAMEL_CASE" style',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'src/utils/CALCULATE_PRICE.js',
+        options: [{ '**/*.js': 'CAMEL_CASE', '**/*.jsx': 'CAMEL_CASE' }],
+        errors: [
+          {
+            message:
+              'The filename "src/utils/CALCULATE_PRICE.js" does not match the "CAMEL_CASE" style',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+    ],
+  }
+);
