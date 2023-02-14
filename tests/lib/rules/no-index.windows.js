e09eaade249d2f883aba35130595e75c5412c902
Duke Luo <dukeluo@outlook.com>
2023-02-15 00:33:14 +0800

    test: add unit cases on Windows for rule no-index

---
+++ tests/lib/rules/no-index.windows.js
@@ -0,0 +1,54 @@
+/**
+ * @file A file cannot be named "index"
+ * @author Duke Luo
+ */
+'use strict';
+
+const path = require('path');
+const proxyquire = require('proxyquire');
+const RuleTester = require('eslint').RuleTester;
+
+const rule = proxyquire('../../../lib/rules/no-index', {
+  path: { ...path.win32, '@global': true },
+});
+const ruleTester = new RuleTester();
+
+ruleTester.run('no-index on Windows', rule, {
+  valid: [
+    {
+      code: "var foo = 'bar';",
+      filename: 'C:\\Users\\Administrator\\Downloads\\wai\\src\\main.js',
+    },
+    {
+      code: "var foo = 'bar';",
+      filename: 'src\\main.ts',
+    },
+  ],
+
+  invalid: [
+    {
+      code: "var foo = 'bar';",
+      filename: 'C:\\Users\\Administrator\\Downloads\\wai\\src\\index.js',
+      errors: [
+        {
+          message:
+            'The filename "index" is not allowed, please use another one',
+          column: 1,
+          line: 1,
+        },
+      ],
+    },
+    {
+      code: "var foo = 'bar';",
+      filename: 'src\\index.ts',
+      errors: [
+        {
+          message:
+            'The filename "index" is not allowed, please use another one',
+          column: 1,
+          line: 1,
+        },
+      ],
+    },
+  ],
+});
