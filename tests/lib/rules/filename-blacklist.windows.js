3069c4b97f3a049a7154142191892ac8abe11e22
Duke Luo <dukeluo@outlook.com>
2023-02-20 22:15:08 +0800

    test: add unit cases on Windows for filename-blacklist rule

---
+++ tests/lib/rules/filename-blacklist.windows.js
@@ -0,0 +1,108 @@
+/**
+ * @file The filename should follow the filename naming convention
+ * @author Duke Luo
+ */
+'use strict';
+
+const path = require('path');
+const proxyquire = require('proxyquire');
+const RuleTester = require('eslint').RuleTester;
+
+const rule = proxyquire('../../../lib/rules/filename-blacklist', {
+  path: { ...path.win32, '@global': true },
+});
+const ruleTester = new RuleTester();
+
+ruleTester.run(
+  "filename-blacklist with option on Windows: [{ '*.models.ts': '*.model.ts' }]",
+  rule,
+  {
+    valid: [
+      {
+        code: "var foo = 'bar';",
+        filename: 'C:\\Users\\Administrator\\Downloads\\wai\\src\\foo.model.ts',
+        options: [{ '*.models.ts': '*.model.ts' }],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'src\\foo.model.ts',
+        options: [{ '*.models.ts': '*.model.ts' }],
+      },
+    ],
+    invalid: [
+      {
+        code: "var foo = 'bar';",
+        filename:
+          'C:\\Users\\Administrator\\Downloads\\wai\\src\\foo.models.ts',
+        options: [{ '*.models.ts': '*.model.ts' }],
+        errors: [
+          {
+            message:
+              'The filename "foo.models.ts" matches the blacklisted "*.models.ts" pattern. Use a pattern like "*.model.ts" instead.',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'src\\foo.models.ts',
+        options: [{ '*.models.ts': '*.model.ts' }],
+        errors: [
+          {
+            message:
+              'The filename "foo.models.ts" matches the blacklisted "*.models.ts" pattern. Use a pattern like "*.model.ts" instead.',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+    ],
+  }
+);
+
+ruleTester.run(
+  "filename-blacklist with option on Windows: [{ '*.models.ts': 'FOO' }]",
+  rule,
+  {
+    valid: [],
+
+    invalid: [
+      {
+        code: "var foo = 'bar';",
+        filename: 'src\\foo.models.ts',
+        options: [{ '*.models.ts': 'FOO' }],
+        errors: [
+          {
+            message: 'There is an invalid pattern "FOO", please check it',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+    ],
+  }
+);
+
+ruleTester.run(
+  "filename-blacklist with option on Windows: [{ 'models.ts': '*.model.ts' }]",
+  rule,
+  {
+    valid: [],
+
+    invalid: [
+      {
+        code: "var foo = 'bar';",
+        filename: 'src\\foo.models.ts',
+        options: [{ 'models.ts': '*.model.ts' }],
+        errors: [
+          {
+            message: 'There is an invalid pattern "models.ts", please check it',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+    ],
+  }
+);
