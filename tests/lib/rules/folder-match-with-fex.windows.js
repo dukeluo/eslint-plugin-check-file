b03f0826303f04c56a6522518b25a2f07a797167
Duke Luo <dukeluo@outlook.com>
2023-02-25 17:25:23 +0800

    test: add unit cases on Windows for folder-match-with-fex rule

---
+++ tests/lib/rules/folder-match-with-fex.windows.js
@@ -0,0 +1,309 @@
+/**
+ * @file The folder should match the naming pattern specified by the file extension
+ * @author Duke Luo
+ */
+'use strict';
+
+const path = require('path');
+const proxyquire = require('proxyquire');
+const RuleTester = require('eslint').RuleTester;
+
+const rule = proxyquire('../../../lib/rules/folder-match-with-fex', {
+  path: { ...path.win32, '@global': true },
+});
+const ruleTester = new RuleTester();
+
+ruleTester.run(
+  "folder-match-with-fex with option on Windows: [{ '*.js': '**/__tests__/' }]",
+  rule,
+  {
+    valid: [
+      {
+        code: "var foo = 'bar';",
+        filename: 'C:\\__tests__\\foo.test.js',
+        options: [{ '*.js': '**/__tests__/' }],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: '__tests__\\foo.test.js',
+        options: [{ '*.js': '**/__tests__/' }],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'bar\\__tests__\\foo.test.js',
+        options: [{ '*.js': '**/__tests__/' }],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'C:\\bar\\__tests__\\foo.test.js',
+        options: [{ '*.js': '**/__tests__/' }],
+      },
+    ],
+
+    invalid: [
+      {
+        code: "var foo = 'bar';",
+        filename: 'C:\\bar\\__test__\\foo.test.js',
+        options: [{ '*.js': '**/__tests__/' }],
+        errors: [
+          {
+            message:
+              'The folder of the file "bar/__test__/foo.test.js" does not match "**/__tests__/"',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+    ],
+  }
+);
+
+ruleTester.run(
+  "folder-match-with-fex with option on Windows: [{ '*.js': '*/__tests__/' }]",
+  rule,
+  {
+    valid: [
+      {
+        code: "var foo = 'bar';",
+        filename: 'bar\\__tests__\\foo.test.js',
+        options: [{ '*.js': '*/__tests__/' }],
+      },
+    ],
+
+    invalid: [
+      {
+        code: "var foo = 'bar';",
+        filename: 'C:\\__tests__\\foo.test.js',
+        options: [{ '*.js': '*/__tests__/' }],
+        errors: [
+          {
+            message:
+              'The folder of the file "__tests__/foo.test.js" does not match "*/__tests__/"',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'C:\\__test__\\foo.test.js',
+        options: [{ '*.js': '*/__tests__/' }],
+        errors: [
+          {
+            message:
+              'The folder of the file "__test__/foo.test.js" does not match "*/__tests__/"',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: '__tests__\\foo.test.js',
+        options: [{ '*.js': '*/__tests__/' }],
+        errors: [
+          {
+            message:
+              'The folder of the file "__tests__/foo.test.js" does not match "*/__tests__/"',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+    ],
+  }
+);
+
+ruleTester.run(
+  "folder-match-with-fex with option on Windows: [{ '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }]",
+  rule,
+  {
+    valid: [
+      {
+        code: "var foo = 'bar';",
+        filename: 'bar\\__tests__\\foo.test.js',
+        options: [{ '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'bar\\__tests__\\foo.test.jsx',
+        options: [{ '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'bar\\__tests__\\foo.test.ts',
+        options: [{ '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'bar\\__tests__\\foo.test.tsx',
+        options: [{ '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
+      },
+    ],
+
+    invalid: [
+      {
+        code: "var foo = 'bar';",
+        filename: 'bar\\_tests_\\foo.test.js',
+        options: [{ '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
+        errors: [
+          {
+            message:
+              'The folder of the file "bar/_tests_/foo.test.js" does not match "*/__tests__/"',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'bar\\_tests_\\foo.test.jsx',
+        options: [{ '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
+        errors: [
+          {
+            message:
+              'The folder of the file "bar/_tests_/foo.test.jsx" does not match "*/__tests__/"',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'bar\\_tests_\\foo.test.ts',
+        options: [{ '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
+        errors: [
+          {
+            message:
+              'The folder of the file "bar/_tests_/foo.test.ts" does not match "*/__tests__/"',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'bar\\_tests_\\foo.test.tsx',
+        options: [{ '*.test.{js,jsx,ts,tsx}': '*/__tests__/' }],
+        errors: [
+          {
+            message:
+              'The folder of the file "bar/_tests_/foo.test.tsx" does not match "*/__tests__/"',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+    ],
+  }
+);
+
+ruleTester.run(
+  "folder-match-with-fex with option on Windows: [{ '*.test.js': '*/__tests__/', '*.test.ts': '*/__tests__/' }]",
+  rule,
+  {
+    valid: [
+      {
+        code: "var foo = 'bar';",
+        filename: 'bar\\__tests__\\foo.test.js',
+        options: [{ '*.test.js': '*/__tests__/', '*.test.ts': '*/__tests__/' }],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'bar\\__tests__\\foo.test.ts',
+        options: [{ '*.test.js': '*/__tests__/', '*.test.ts': '*/__tests__/' }],
+      },
+    ],
+
+    invalid: [
+      {
+        code: "var foo = 'bar';",
+        filename: 'bar\\_tests_\\foo.test.js',
+        options: [{ '*.test.js': '*/__tests__/', '*.test.ts': '*/__tests__/' }],
+        errors: [
+          {
+            message:
+              'The folder of the file "bar/_tests_/foo.test.js" does not match "*/__tests__/"',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'bar\\_tests_\\foo.test.ts',
+        options: [{ '*.test.js': '*/__tests__/', '*.test.ts': '*/__tests__/' }],
+        errors: [
+          {
+            message:
+              'The folder of the file "bar/_tests_/foo.test.ts" does not match "*/__tests__/"',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+    ],
+  }
+);
+
+ruleTester.run(
+  'folder-match-with-fex with fex that has not been set on Windows',
+  rule,
+  {
+    valid: [
+      {
+        code: "var foo = 'bar';",
+        filename: 'C:\\bar\\__test__\\foo.test.ts',
+        options: [{ '*.js': '**/__tests__/', '*.jsx': '**/__tests__/' }],
+      },
+    ],
+
+    invalid: [],
+  }
+);
+
+ruleTester.run(
+  "folder-match-with-fex with option on Windows: [{ '*.test.js': 'FOO', '*.test.ts': '*/__tests__/' }]",
+  rule,
+  {
+    valid: [],
+
+    invalid: [
+      {
+        code: "var foo = 'bar';",
+        filename: 'bar\\__tests__\\foo.test.js',
+        options: [{ '*.test.js': 'FOO', '*.test.ts': '*/__tests__/' }],
+        errors: [
+          {
+            message: 'There is an invalid pattern "FOO", please check it',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+    ],
+  }
+);
+
+ruleTester.run(
+  "folder-match-with-fex with option on Windows: [{ '*.test.js': '*/__tests__/', '.test.ts': '*/__tests__/' }]",
+  rule,
+  {
+    valid: [],
+
+    invalid: [
+      {
+        code: "var foo = 'bar';",
+        filename: 'bar\\__tests__\\foo.test.js',
+        options: [{ '*.test.js': '*/__tests__/', '.test.ts': '*/__tests__/' }],
+        errors: [
+          {
+            message: 'There is an invalid pattern ".test.ts", please check it',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+    ],
+  }
+);
