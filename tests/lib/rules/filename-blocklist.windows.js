5ef63eb172a8a255f90f25dda723b9bcdcacb210
Jeremey Key <jeremeykey@gmail.com>
2023-03-21 16:26:59 -0400

    fix: update filename-blocklist to check filename path

--- tests/lib/rules/filename-blocklist.windows.js
+++ tests/lib/rules/filename-blocklist.windows.js
@@ -14,19 +14,19 @@ const rule = proxyquire('../../../lib/rules/filename-blocklist', {
 const ruleTester = new RuleTester();
 
 ruleTester.run(
-  "filename-blocklist with option on Windows: [{ '*.models.ts': '*.model.ts' }]",
+  "filename-blocklist with option on Windows: [{ '**/*.models.ts': '**/*.model.ts' }]",
   rule,
   {
     valid: [
       {
         code: "var foo = 'bar';",
         filename: 'C:\\Users\\Administrator\\Downloads\\wai\\src\\foo.model.ts',
-        options: [{ '*.models.ts': '*.model.ts' }],
+        options: [{ '**/*.models.ts': '**/*.model.ts' }],
       },
       {
         code: "var foo = 'bar';",
         filename: 'src\\foo.model.ts',
-        options: [{ '*.models.ts': '*.model.ts' }],
+        options: [{ '**/*.models.ts': '**/*.model.ts' }],
       },
     ],
     invalid: [
@@ -34,11 +34,11 @@ ruleTester.run(
         code: "var foo = 'bar';",
         filename:
           'C:\\Users\\Administrator\\Downloads\\wai\\src\\foo.models.ts',
-        options: [{ '*.models.ts': '*.model.ts' }],
+        options: [{ '**/*.models.ts': '**/*.model.ts' }],
         errors: [
           {
             message:
-              'The filename "foo.models.ts" matches the blocklisted "*.models.ts" pattern. Use a pattern like "*.model.ts" instead.',
+              'The filename "foo.models.ts" matches the blocklisted "**/*.models.ts" pattern. Use a pattern like "**/*.model.ts" instead.',
             column: 1,
             line: 1,
           },
@@ -47,11 +47,45 @@ ruleTester.run(
       {
         code: "var foo = 'bar';",
         filename: 'src\\foo.models.ts',
-        options: [{ '*.models.ts': '*.model.ts' }],
+        options: [{ '**/*.models.ts': '**/*.model.ts' }],
         errors: [
           {
             message:
-              'The filename "foo.models.ts" matches the blocklisted "*.models.ts" pattern. Use a pattern like "*.model.ts" instead.',
+              'The filename "foo.models.ts" matches the blocklisted "**/*.models.ts" pattern. Use a pattern like "**/*.model.ts" instead.',
+            column: 1,
+            line: 1,
+          },
+        ],
+      },
+    ],
+  }
+);
+
+ruleTester.run(
+  "filename-blocklist with option on Windows: [{ 'src/**/*.models.ts': 'src/**/*.model.ts' }]",
+  rule,
+  {
+    valid: [
+      {
+        code: "var foo = 'bar';",
+        filename: 'C:\\Users\\Administrator\\Downloads\\wai\\src\\foo.model.ts',
+        options: [{ 'src/**/*.models.ts': 'src/**/*.model.ts' }],
+      },
+      {
+        code: "var foo = 'bar';",
+        filename: 'src\\foo.model.ts',
+        options: [{ 'src/**/*.models.ts': 'src/**/*.model.ts' }],
+      },
+    ],
+    invalid: [
+      {
+        code: "var foo = 'bar';",
+        filename: 'src\\foo.models.ts',
+        options: [{ 'src/**/*.models.ts': 'src/**/*.model.ts' }],
+        errors: [
+          {
+            message:
+              'The filename "foo.models.ts" matches the blocklisted "src/**/*.models.ts" pattern. Use a pattern like "src/**/*.model.ts" instead.',
             column: 1,
             line: 1,
           },
