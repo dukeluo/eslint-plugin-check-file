691ab9a4048481bccab95c0842c8f50bcccd03df
Huan Luo <dukeluo@outlook.com>
2023-07-19 22:39:22 +0800

    chore: bundle package with rollup

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+/**
+ * @file Rollup configuration.
+ * @author Huan Luo
+ */
+
+import terser from '@rollup/plugin-terser';
+
+export default {
+  input: 'lib/index.js',
+  output: [
+    {
+      format: 'cjs',
+      file: 'dist/index.js',
+      banner:
+        '/*! @author Huan Luo <dukeluo@outlook.com> (https://shaiwang.life) */',
+    },
+    {
+      format: 'es',
+      file: 'dist/index.es.js',
+      banner:
+        '/*! @author Huan Luo <dukeluo@outlook.com> (https://shaiwang.life) */',
+    },
+  ],
+  plugins: [terser()],
+  external: ['is-glob', 'micromatch', 'path'],
+};
