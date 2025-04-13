730cce767b664cc9f04bcefb15bc972785d44ac6
Huan Luo <dukeluo@outlook.com>
2025-04-13 14:59:48 +0800

    chore(#57): add type files

--- rollup.config.js
+++ rollup.config.js
@@ -4,23 +4,44 @@
  */
 
 import terser from '@rollup/plugin-terser';
+import dts from 'rollup-plugin-dts';
 
-export default {
-  input: 'lib/index.js',
-  output: [
-    {
-      format: 'cjs',
-      file: 'dist/index.cjs',
-      banner:
-        '/*! @author Huan Luo <dukeluo@outlook.com> (https://shaiwang.life) */',
-    },
-    {
-      format: 'es',
-      file: 'dist/index.js',
-      banner:
-        '/*! @author Huan Luo <dukeluo@outlook.com> (https://shaiwang.life) */',
-    },
-  ],
-  plugins: [terser()],
-  external: ['is-glob', 'micromatch', 'path'],
-};
+export default [
+  {
+    input: 'lib/index.js',
+    output: [
+      {
+        format: 'cjs',
+        file: 'dist/index.cjs',
+        banner:
+          '/*! @author Huan Luo <dukeluo@outlook.com> (https://shaiwang.life) */',
+      },
+      {
+        format: 'es',
+        file: 'dist/index.js',
+        banner:
+          '/*! @author Huan Luo <dukeluo@outlook.com> (https://shaiwang.life) */',
+      },
+    ],
+    plugins: [terser()],
+    external: ['is-glob', 'micromatch', 'path'],
+  },
+  {
+    input: 'lib/index.js',
+    output: [
+      {
+        format: 'cjs',
+        file: 'dist/index.d.cts',
+        banner:
+          '/*! @author Huan Luo <dukeluo@outlook.com> (https://shaiwang.life) */',
+      },
+      {
+        format: 'es',
+        file: 'dist/index.d.ts',
+        banner:
+          '/*! @author Huan Luo <dukeluo@outlook.com> (https://shaiwang.life) */',
+      },
+    ],
+    plugins: [dts()],
+  },
+];
